eaa6247f1fcb3801438b6fbdbf719d58c1f9e634
Uwase Djaria <uwasedjaria41@gmail.com>
2025-10-27 14:53:41 +0200

    create folder with npm install

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import ToDoListForm from './components/ToDoListForm'
+
+function App() {
+
+
+  return (
+    
+      <div>
+       <ToDoListForm/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/ToDoListForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ToDoListForm() {
+  return (
+    <div className="flex-1 flex-shrink flex-grow justify-center  font-sans">
+        <h1 >To Do List</h1>
+    </div>
+  )
+}
+
+export default ToDoListForm
