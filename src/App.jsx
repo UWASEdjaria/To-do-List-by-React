b1f9e7639d0491bfde9aeb1df0e445e667a25797
Uwase Djaria <uwasedjaria41@gmail.com>
2025-10-27 15:27:48 +0200

    add tailwindcss and craete the add button

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import './index.css'
 import ToDoListForm from './components/ToDoListForm'
 
 function App() {
--- src/components/ToDoListForm.jsx
+++ src/components/ToDoListForm.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 
 function ToDoListForm() {
   return (
-    <div className="flex-1 flex-shrink flex-grow justify-center  font-sans">
-        <h1 >To Do List</h1>
-    </div>
+    <section className="flex w-full flex-col justify-center items-center min-h-screen mt-5 font-sans">
+
+        {/* ToDoList form */}
+
+        <form className='w-80 h-60 text-white'>
+            <h1 className='text-center p-5 mb-2 text-2xl sm:text-2xl md:text-3xl lg:text-4xl font-bold  font-sans transition-all duration-200 hover:scale-105'>To Do List</h1>
+            <input className='border w-72 p-2  rounded-lg'
+            type='text'
+            placeholder='Enter task ......'/>
+
+           <button>Add</button>
+
+        </form>
+    </section>
   )
 }
 
